52776e38117a019eba6b85aeb91ac44b63f840d5
Sahil9214 <utkarshsinghal369@gmail.com>
2022-12-15 10:56:05 +0530

    day2 work

--- naukrikare/src/Components/Part2.jsx
+++ naukrikare/src/Components/Part2.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text } from "@chakra-ui/react";
 import { Input, Select, Button } from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 import { SearchIcon } from "@chakra-ui/icons";
 const Part2 = () => {
   return (
@@ -74,7 +75,7 @@ const Part2 = () => {
             <Select
               style={{ marginLeft: "4px" }}
               placeholder="Select Experience"
-            size="lg"
+              size="lg"
             >
               <option value="option1">Fresher</option>
               <option value="option2">1 year</option>
@@ -110,17 +111,520 @@ const Part2 = () => {
               fontSize: "19px",
               fontWeight: "600",
               cursor: "pointer",
-
             }}
           >
             Search
           </Button>
         </div>
       </div>
-      <br/>
-      <div>
-        {/* /// here i have to add differnet component button */}
-</div>
+
+      <div
+        style={{
+          width: "80%",
+          margin: "auto",
+          display: "flex",
+          marginTop: "80px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            boxShadow: "0px 3px 6px rgb(0 77 255 / 5%)",
+            justifyContent: "center",
+            border: " 1px solid #EAF1F5",
+            borderRadius: "16px 6px",
+            background: "#FFFFFF",
+            padding: "0 12px",
+            alignItems: "center",
+            marginRight: "14px",
+            height: "64px",
+          }}
+        >
+          <img
+            src="https://static.naukimg.com/s/0/0/i/trending-naukri/remote.svg"
+            width="40px"
+            height="40px"
+            alt="Remote-img"
+          />
+          <span
+            style={{
+              fontWeight: "500",
+              fontSize: "17px",
+              lineHeight: "22px",
+              color: " #1B2437",
+              margin: "10px",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              overflow: " hidden",
+              maxWidth: "95px",
+            }}
+          >
+            Remote
+          </span>
+          <ChevronRightIcon
+            style={{
+              width: "25px",
+              height: "25px",
+              position: "relative",
+              // marginTop: "10px",
+            }}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            boxShadow: "0px 3px 6px rgb(0 77 255 / 5%)",
+            justifyContent: "center",
+            border: " 1px solid #EAF1F5",
+            borderRadius: "16px 6px",
+            background: "#FFFFFF",
+            padding: "0 12px",
+            alignItems: "center",
+            marginRight: "14px",
+            height: "64px",
+          }}
+        >
+          <img
+            class="ntc__chip-logo"
+            src="https://static.naukimg.com/s/0/0/i/trending-naukri/mnc.svg"
+            width="40px"
+            height="40px"
+            alt="Remote-img"
+          />
+          <span
+            style={{
+              fontWeight: "500",
+              fontSize: "17px",
+              lineHeight: "22px",
+              color: " #1B2437",
+              margin: "10px",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              overflow: " hidden",
+              maxWidth: "95px",
+            }}
+          >
+            MNC
+          </span>
+          <ChevronRightIcon
+            style={{
+              width: "25px",
+              height: "25px",
+              position: "relative",
+              // marginTop: "10px",
+            }}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            boxShadow: "0px 3px 6px rgb(0 77 255 / 5%)",
+            justifyContent: "center",
+            border: " 1px solid #EAF1F5",
+            borderRadius: "16px 6px",
+            background: "#FFFFFF",
+            padding: "0 12px",
+            alignItems: "center",
+            marginRight: "14px",
+            height: "64px",
+          }}
+        >
+          <img
+            class="ntc__chip-logo"
+            src="https://static.naukimg.com/s/0/0/i/trending-naukri/analytics.svg"
+            width="40px"
+            height="40px"
+            alt="Remote-img"
+          />
+          <span
+            style={{
+              fontWeight: "500",
+              fontSize: "17px",
+              lineHeight: "22px",
+              color: " #1B2437",
+              margin: "10px",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              overflow: " hidden",
+              maxWidth: "95px",
+            }}
+          >
+            Analytics
+          </span>
+          <ChevronRightIcon
+            style={{
+              width: "25px",
+              height: "25px",
+              position: "relative",
+              // marginTop: "10px",
+            }}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            boxShadow: "0px 3px 6px rgb(0 77 255 / 5%)",
+            justifyContent: "center",
+            border: " 1px solid #EAF1F5",
+            borderRadius: "16px 6px",
+            background: "#FFFFFF",
+            padding: "0 12px",
+            alignItems: "center",
+            marginRight: "14px",
+            height: "64px",
+          }}
+        >
+          <img
+            src="https://static.naukimg.com/s/0/0/i/trending-naukri/engineering.svg"
+            width="40px"
+            height="40px"
+            alt="Remote-img"
+          />
+          <span
+            style={{
+              fontWeight: "500",
+              fontSize: "17px",
+              lineHeight: "22px",
+              color: " #1B2437",
+              margin: "10px",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              overflow: " hidden",
+              maxWidth: "95px",
+            }}
+          >
+            Enginerring
+          </span>
+          <ChevronRightIcon
+            style={{
+              width: "25px",
+              height: "25px",
+              position: "relative",
+              // marginTop: "10px",
+            }}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            boxShadow: "0px 3px 6px rgb(0 77 255 / 5%)",
+            justifyContent: "center",
+            border: " 1px solid #EAF1F5",
+            borderRadius: "16px 6px",
+            background: "#FFFFFF",
+            padding: "0 12px",
+            alignItems: "center",
+            marginRight: "14px",
+            height: "64px",
+          }}
+        >
+          <img
+            src="https://static.naukimg.com/s/0/0/i/trending-naukri/project-management.svg"
+            width="40px"
+            height="40px"
+            alt="Remote-img"
+          />
+          <span
+            style={{
+              fontWeight: "500",
+              fontSize: "17px",
+              lineHeight: "22px",
+              color: " #1B2437",
+              margin: "10px",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              overflow: " hidden",
+              maxWidth: "95px",
+            }}
+          >
+            Projext Managment
+          </span>
+          <ChevronRightIcon
+            style={{
+              width: "25px",
+              height: "25px",
+              position: "relative",
+              // marginTop: "10px",
+            }}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            boxShadow: "0px 3px 6px rgb(0 77 255 / 5%)",
+            justifyContent: "center",
+            border: " 1px solid #EAF1F5",
+            borderRadius: "16px 6px",
+            background: "#FFFFFF",
+            padding: "0 12px",
+            alignItems: "center",
+            marginRight: "14px",
+            height: "64px",
+          }}
+        >
+          <img
+            src="https://static.naukimg.com/s/0/0/i/trending-naukri/hr.svg"
+            width="40px"
+            height="40px"
+            alt="Remote-img"
+          />
+          <span
+            style={{
+              fontWeight: "500",
+              fontSize: "17px",
+              lineHeight: "22px",
+              color: " #1B2437",
+              margin: "10px",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              overflow: " hidden",
+              maxWidth: "95px",
+            }}
+          >
+            HR
+          </span>
+          <ChevronRightIcon
+            style={{
+              width: "25px",
+              height: "25px",
+              position: "relative",
+              // marginTop: "10px",
+            }}
+          />
+        </div>
+      </div>
+      {/* //ANother buttons or u can say component sections  */}
+      <div style={{ width: "60%", margin: "auto", display: "flex" ,marginTop:"40px" }}>
+        <div
+          style={{
+            display: "flex",
+            boxShadow: "0px 3px 6px rgb(0 77 255 / 5%)",
+            justifyContent: "center",
+            border: " 1px solid #EAF1F5",
+            borderRadius: "16px 6px",
+            background: "#FFFFFF",
+            padding: "0 12px",
+            alignItems: "center",
+            marginRight: "14px",
+            height: "64px",
+            width:"220px"
+          }}
+        >
+          <img
+            class="ntc__chip-logo"
+            src="https://static.naukimg.com/s/0/0/i/trending-naukri/temporary-wfh.svg"
+            width="40px"
+            height="40px"
+            alt="Remote-img"
+          />
+          <span
+            style={{
+              fontWeight: "500",
+              fontSize: "17px",
+              lineHeight: "22px",
+              color: " #1B2437",
+              margin: "10px",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              overflow: " hidden",
+              maxWidth: "95px",
+            }}
+          >
+            Temp WFH
+          </span>
+          <ChevronRightIcon
+            style={{
+              width: "25px",
+              height: "25px",
+              position: "relative",
+              // marginTop: "10px",
+            }}
+          />
+        </div>
+        {/* //Banking && */}
+        <div
+          style={{
+            display: "flex",
+            boxShadow: "0px 3px 6px rgb(0 77 255 / 5%)",
+            justifyContent: "center",
+            border: " 1px solid #EAF1F5",
+            borderRadius: "16px 6px",
+            background: "#FFFFFF",
+            padding: "0 12px",
+            alignItems: "center",
+            marginRight: "14px",
+            height: "64px",
+          }}
+        >
+          <img
+            class="ntc__chip-logo"
+            src="https://static.naukimg.com/s/0/0/i/trending-naukri/finance.svg"
+            width="40px"
+            height="40px"
+            alt="Remote-img"
+          />
+          <span
+            style={{
+              fontWeight: "500",
+              fontSize: "17px",
+              lineHeight: "22px",
+              color: " #1B2437",
+              margin: "10px",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              overflow: " hidden",
+              maxWidth: "95px",
+            }}
+          >
+            Banking &....
+          </span>
+          <ChevronRightIcon
+            style={{
+              width: "25px",
+              height: "25px",
+              position: "relative",
+              // marginTop: "10px",
+            }}
+          />
+        </div>
+        {/* Supplu chain */}
+        <div
+          style={{
+            display: "flex",
+            boxShadow: "0px 3px 6px rgb(0 77 255 / 5%)",
+            justifyContent: "center",
+            border: " 1px solid #EAF1F5",
+            borderRadius: "16px 6px",
+            background: "#FFFFFF",
+            padding: "0 12px",
+            alignItems: "center",
+            marginRight: "14px",
+            height: "64px",
+          }}
+        >
+          <img
+            class="ntc__chip-logo"
+            src="https://static.naukimg.com/s/0/0/i/trending-naukri/supplychain.svg"
+            width="40px"
+            height="40px"
+            alt="Remote-img"
+          />
+          <span
+            style={{
+              fontWeight: "500",
+              fontSize: "17px",
+              lineHeight: "22px",
+              color: " #1B2437",
+              margin: "10px",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              overflow: " hidden",
+              maxWidth: "95px",
+            }}
+          >
+            Supply chain
+          </span>
+          <ChevronRightIcon
+            style={{
+              width: "25px",
+              height: "25px",
+              position: "relative",
+              // marginTop: "10px",
+            }}
+          />
+        </div>
+        {/* software */}
+        <div
+          style={{
+            display: "flex",
+            boxShadow: "0px 3px 6px rgb(0 77 255 / 5%)",
+            justifyContent: "center",
+            border: " 1px solid #EAF1F5",
+            borderRadius: "16px 6px",
+            background: "#FFFFFF",
+            padding: "0 12px",
+            alignItems: "center",
+            marginRight: "14px",
+            height: "64px",
+          }}
+        >
+          <img
+            class="ntc__chip-logo"
+            src="https://static.naukimg.com/s/0/0/i/trending-naukri/software.svg"
+            height="40px"
+            alt="Remote-img"
+          />
+          <span
+            style={{
+              fontWeight: "500",
+              fontSize: "17px",
+              lineHeight: "22px",
+              color: " #1B2437",
+              margin: "10px",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              overflow: " hidden",
+              maxWidth: "95px",
+            }}
+          >
+            Software
+          </span>
+          <ChevronRightIcon
+            style={{
+              width: "25px",
+              height: "25px",
+              position: "relative",
+              // marginTop: "10px",
+            }}
+          />
+        </div>
+        {/* //fresher */}
+        <div
+          style={{
+            display: "flex",
+            boxShadow: "0px 3px 6px rgb(0 77 255 / 5%)",
+            justifyContent: "center",
+            border: " 1px solid #EAF1F5",
+            borderRadius: "16px 6px",
+            background: "#FFFFFF",
+            padding: "0 12px",
+            alignItems: "center",
+            marginRight: "14px",
+            height: "64px",
+          }}
+        >
+          <img
+            class="ntc__chip-logo"
+            src="https://static.naukimg.com/s/0/0/i/trending-naukri/freshers.svg"
+            width="40px"
+            height="40px"
+            alt="Remote-img"
+          />
+          <span
+            style={{
+              fontWeight: "500",
+              fontSize: "17px",
+              lineHeight: "22px",
+              color: " #1B2437",
+              margin: "10px",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              overflow: " hidden",
+              maxWidth: "95px",
+            }}
+          >
+            Fresher
+          </span>
+          <ChevronRightIcon
+            style={{
+              width: "25px",
+              height: "25px",
+              position: "relative",
+              // marginTop: "10px",
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
